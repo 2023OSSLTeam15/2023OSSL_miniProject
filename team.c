a01968fadd9569ea2f2a73b3e312b25e0a2352c1
hs22200250 <130723631+oss22200250@users.noreply.github.com>
2023-05-11 18:48:52 +0900

    Modify team.c
    create functions (add, read, list, update, search. . .)

--- team.c
+++ team.c
@@ -9,11 +9,97 @@ int selectMenu(){
     printf("4. 목록 삭제\n");
     printf("5. 파일 저장\n");
     printf("6. 교회 검색\n");
-    printf("7. 이번 주 교회 출석 현황\n");
-    printf("6. 이번 달 교회 출석 현황\n");
-    printf("6. 오늘의 추천 교회는?\n");
+    //printf("7. 이번 주 교회 출석 현황\n");
+    //printf("8. 이번 달 교회 출석 현황\n");
+    //printf("9. 오늘의 추천 교회는?\n");
     printf("0. 종료\n\n");
     printf(">> 원하는 메뉴를 골라주세요:  ");
     scanf("%d", &menu);
     return menu;
 }
+
+int addBoard(Church *c){
+    printf("교회 이름은?  ");
+    scanf("%s", c->name);
+    printf("교회 주소는?  ");
+    scanf("%s", c->address);
+    printf("청년부 예배 시간은(00:00 형태로 표기)?  ");
+    scanf("%s", c->time);
+    printf("=> 추가됨!\n\n");
+    return 1;
+}
+
+void listBoard(Church *c[], int count){
+    printf("\nNo   Name   Address \t time\n");
+    printf("=====================================\n);
+    for(int i=0; i<count; i++){
+        if(c[i]==NULL) continue;
+        printf("%2d ", i+1);
+        readBoard(*c[i]);
+    }
+    printf("\n");
+}
+
+void readBoard(Church c){
+    printf(" %s  %s \t %s\n", c.name, c.address, c.time);
+}
+
+int selectDataNo(Church *c[], int count){
+    int no;
+    listBoard(c, count);
+    printf("번호는 (취소: 0)? ");
+    scanf("%d", &no);
+    return no;
+}
+
+int updateBoard(Church *c){
+    printf("교회 이름은?  ");
+    scanf("%s", c->name);
+    printf("교회 주소는?  ");
+    scanf("%s", c->address);
+    printf("청년부 예배 시간은(00:00 형태로 표기)?  ");
+    scanf("%s", c->time);
+    printf("=> 수정됨!\n");
+    return 1;
+}
+
+int deleteBoard(Church *c){}
+
+void searchBoard(Church **c, int count){
+    int scnt = 0;
+    char search[50];
+    
+    printf("검색할 교회 이름?  ");
+    scanf("%s", search);
+    
+    printf("\nNo   Name   Address \t time\n");
+    printf("=====================================\n);
+    for(int i=0; i<count; i++){
+        if(c[i] == NULL) continue;
+        if(strstr(c[i]->name, search)){
+            printf("%2d ", i+1);
+            readBoard(*c[i]);
+            scnt++;
+        }
+    }
+    if(scnt == 0) printf("=> 검색된 데이터 없음!");
+    printf("\n");
+}
+
+
+
+void saveBoard(Church **c, int count){}
+
+int loadBoard(Church **c){}
+
+
+
+int attendance(){}
+
+void thisWeek(){}
+
+void thisMonth(){}
+
+int isSunday(){}
+
+void recommendChurch(Church *c[], int count){}
