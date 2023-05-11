b9fb4d9f5fe5d75a1b2cc96bbaea5dda75057d54
hs22200250 <130723631+oss22200250@users.noreply.github.com>
2023-05-11 18:22:38 +0900

    Modify team.h
    check parameters of functions,
    create 'listBoard', 'selectDataNo' functions.

--- team.h
+++ team.h
@@ -15,14 +15,16 @@ typedef struct{
 
 int selectMenu();
 
-int addBoard(Church *c);
-void readBoard(Church *c[], int count);
-int updateBoard(Church *c);
-int deleteBoard(Church *c);
-void searchBoard(Church *c[], int count);
+int addBoard(Church *c);    //Create
+void listBoard(Church *c[], int count); //Read
+void readBoard(Church c);
+int selectDataNo(Church *c[], int count);   //Selecting number (for Update, Delete)
+int updateBoard(Church *c); //Update
+int deleteBoard(Church *c); //Delete
+void searchBoard(Church **c, int count);
 
-void saveBoard(Church *c[], int count);
-int loadBoard(Church *c[]);
+void saveBoard(Church **c, int count);
+int loadBoard(Church **c);
 
 int attendance();
 void thisWeek();
